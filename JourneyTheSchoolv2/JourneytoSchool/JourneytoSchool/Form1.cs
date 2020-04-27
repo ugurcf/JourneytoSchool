73e0071dbda86e566cd1ca4e743b67ab6c69a976
ugurcf <ugurcf@users.noreply.github.com>
2020-04-28 02:21:49 +0300

    Journey to School last updated available now.
    last version update v2.

---
+++ JourneyTheSchoolv2/JourneytoSchool/JourneytoSchool/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JourneytoSchool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTrain_Click(object sender, EventArgs e)
+        {
+            TrainForm tf = new TrainForm();
+            tf.Show();
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            TestForm tf = new TestForm();
+            tf.Show();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnTest.TabStop = false;
+            btnTest.FlatStyle = FlatStyle.Flat;
+            btnTest.FlatAppearance.BorderSize = 0;
+            btnTrain.TabStop = false;
+            btnTrain.FlatStyle = FlatStyle.Flat;
+            btnTrain.FlatAppearance.BorderSize = 0;
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ JourneyTheSchoolv2/JourneytoSchool/JourneytoSchool/TestForm.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JourneytoSchool
+{
+    public partial class TestForm : Form
+    {
+        public TestForm()
+        {
+            InitializeComponent();
+        }
+        
+        private void TestForm_Load(object sender, EventArgs e)
+        {
+            btnTest.TabStop = false;
+            btnTest.FlatStyle = FlatStyle.Flat;
+            btnTest.FlatAppearance.BorderSize = 0;
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            int Age =Convert.ToInt32(txtBoxAge.Text);
+            int Dist=Convert.ToInt32(Math.Floor(Convert.ToDecimal(txtBoxDist.Text)));
+            int Friend = Convert.ToInt32(txtBoxFriends.Text);
+            if(Age<=6 || Dist==0)
+            {
+                lblResult.Text = "Not Valid";
+            }
+            if (Dist > 30)
+            {
+                lblResult.Text = "%97.5 Car";
+            }
+            if(Dist<1 && Age>12 && Friend == 0)
+            {
+                lblResult.Text = "%92 Walk";
+            }
+            if (Age>=9 && Age <=11 && Dist < 4)
+            {
+                lblResult.Text = "%67 Car";
+            }
+            if (Age >= 9 && Age <= 11 && Dist < 4 && Dist>=2 && Friend>1 && Friend<=3)
+            {
+                lblResult.Text = "%58 Car";
+            }
+            if (Age >= 9 && Age <= 11 && Dist < 4 && Dist >= 2 && Friend > 4 && Friend <= 20)
+            {
+                lblResult.Text = "%86 Car";
+            }
+            if (Age >= 9 && Age <= 11 && Dist < 4 && Dist>= 2 && Friend >= 0 && Friend <=6)
+            {
+                lblResult.Text = "%75 Car";
+            }
+            if (Age >= 9 && Age <= 11 && Dist <15 && Dist >= 10 && Friend >= 0 && Friend <= 2000)
+            {
+                lblResult.Text = "%96 Car";
+            }
+            
+        }
+    }
+}
