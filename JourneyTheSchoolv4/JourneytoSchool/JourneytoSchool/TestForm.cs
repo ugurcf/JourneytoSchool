8e42ef222c88539a59d5c447b8f0ebfa288cde81
ugurcf <ugurcf@users.noreply.github.com>
2020-04-29 02:15:41 +0300

    v4

---
+++ JourneyTheSchoolv4/JourneytoSchool/JourneytoSchool/TestForm.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JourneytoSchool
+{
+    public partial class TestForm : Form
+    {
+        public TestForm()
+        {
+            InitializeComponent();
+        }
+
+        private void TestForm_Load(object sender, EventArgs e)
+        {
+            btnTest.TabStop = false;
+            btnTest.FlatStyle = FlatStyle.Flat;
+            btnTest.FlatAppearance.BorderSize = 0;
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            int Age = Convert.ToInt32(txtBoxAge.Text);
+            //int Dist=Convert.ToInt32(Math.Floor(Convert.ToDecimal(txtBoxDist.Text)));
+            int Friend = Convert.ToInt32(txtBoxFriends.Text);
+            float Dist = Convert.ToSingle(txtBoxDist.Text);
+            if (Age <= 6)
+            {
+                lblResult.Text = "Not Valid";
+            }
+            if (Age >= 36)
+            {
+                lblResult.Text = "Not Valid";
+            }
+            if (Dist <= 0)// 9 0 2 girince patlıyo
+            {
+                lblResult.Text = "Not Valid";
+            }
+            if (Dist > 30)
+            {
+                lblResult.Text = "%98.5 Car";
+            }
+            if(Age>20 && Age<30 && Dist>0 && Dist<0.9 && Friend > 1){
+                lblResult.Text = "%58.9 Walk";
+            }
+            if (Age > 20 && Age < 30 && Dist > 0 && Dist < 3.9 && Friend >= 1)
+            {
+                lblResult.Text = "%65.9 Car";
+            }
+            if (Age > 20 && Dist >= 4 && Dist <10  && Friend >= 1)
+            {
+                lblResult.Text = "%90.3 Car";
+            }
+            if (Dist <= 1 && Age >= 12 && Age <= 14 && Friend >= 0 && Friend < 4)
+            {
+                lblResult.Text = "%95 Walk";
+            }
+            if (Age >= 9 && Age <= 11 && Dist < 4)
+            {
+                lblResult.Text = "%67 Car";
+            }
+            if (Age >= 9 && Age <= 11 && Dist < 4 && Dist >= 2 && Friend > 1 && Friend <= 3)
+            {
+                lblResult.Text = "%58 Car";
+            }
+            if (Age >= 9 && Age <= 11 && Dist < 4 && Dist >= 2 && Friend > 4 && Friend <= 20)
+            {
+                lblResult.Text = "%86 Car";
+            }
+            if (Age >= 9 && Age <= 11 && Dist < 4 && Dist >= 2 && Friend >= 0 && Friend <= 6)
+            {
+                lblResult.Text = "%75 Car";
+            }
+            if (Age >= 9 && Age <= 11 && Dist < 10 && Dist >= 5 && Friend >= 0 && Friend <= 2000)
+            {
+                lblResult.Text = "%96 Car";
+            }
+            if (Age > 6 && Age <= 7 && Dist > 0 && Dist < 1 && Friend >= 0 && Friend < 2)//yeni eklediklerim
+            {
+                lblResult.Text = "%87 Cycle";
+            }
+            if (Age >= 6 && Age <= 7 && Dist > 0 && Dist < 1 && Friend >= 2 && Friend < 800)
+            {
+                lblResult.Text = "%90 Cycle";
+            }
+            if (Age >= 7 && Age <= 8 && Dist > 0 && Dist < 1 && Friend >= 0 && Friend < 800)
+            {
+                lblResult.Text = "%84 Cycle";
+            }
+            if (Age >= 7 && Age <= 8 && Dist > 1 && Dist < 3 && Friend >= 0 && Friend < 800)
+            {
+                lblResult.Text = "%72 Car";
+            }
+            if (Age >= 7 && Age <= 35 && Dist > 10 && Dist < 300 && Friend >= 0 && Friend < 800)
+            {
+                lblResult.Text = "%99.1 Car";
+            }
+            if (Age >= 8 && Age <= 10 && Dist > 1 && Dist <= 2 && Friend >= 1 && Friend < 3)
+            {
+                lblResult.Text = "%54 Cycle";
+            }
+            if (Age >= 8 && Age <= 9 && Dist >= 0 && Dist <= 1 && Friend >= 0 && Friend < 3)
+            {
+                lblResult.Text = "%61.4 Walk";
+            }
+            if (Age >= 8 && Age <= 9 && Dist >= 0.5 && Dist <= 1 && Friend >= 0 && Friend < 2)
+            {
+                lblResult.Text = "%73.2 Walk";
+            }
+            if (Age >= 8 && Age <= 9 && Dist >= 0.9 && Dist <= 1.8 && Friend >= 4 && Friend < 1500)
+            {
+                lblResult.Text = "%71 Cycle";
+            }
+            if (Age >= 8 && Age <= 9 && Dist >= 0.9 && Dist <= 1.8 && Friend >= 7 && Friend < 1500)
+            {
+                lblResult.Text = "%62 Cycle";
+            }//car için
+            if (Age >= 10 && Age <= 12 && Dist < 4 && Dist >= 1.5)
+            {
+                lblResult.Text = "%72.5 Car";
+            }
+            if (Age >= 10 && Age <= 16 && Dist > 0 && Dist <= 2 && Dist > 1 && Friend > 1 && Friend <= 3)
+            {
+                lblResult.Text = "%54 Car";
+            }
+            if (Age > 9 && Age <= 15 && Dist < 100 && Dist >= 3.6 && Friend >= 0 && Friend <= 2000)
+            {
+                lblResult.Text = "%90.4 Car";
+            }
+            if (Age >= 11 && Age <= 15 && Dist > 2.2 && Dist <= 3)
+            {
+                lblResult.Text = "%82 Car";
+            }
+            if (Age >= 9 && Age <= 16 && Dist < 15 && Dist >= 10 && Friend >= 0 && Friend <= 2000)
+            {
+                lblResult.Text = "%96 Car";
+            }
+            //-----------------------------------
+            //walk için
+            if (Age >= 11 && Age <= 15 && Dist >= 0 && Dist <= 0.9 && Friend == 0)
+            {
+                lblResult.Text = "%62 Walk";
+            }
+            if (Age >= 11 && Age <= 15 && Dist >= 0 && Dist <= 0.8 && Friend >= 1 && Friend < 1500)
+            {
+                lblResult.Text = "%76.6 Walk";
+            }
+            if (Age >= 11 && Age <= 15 && Dist >= 0 && Dist <= 1 && Friend >= 1 && Friend < 1500)
+            {
+                lblResult.Text = "%71 Walk";
+            }
+            if (Age >= 11 && Age <= 15 && Dist >= 0 && Dist <= 1.2 && Friend >= 1 && Friend < 1500)
+            {
+                lblResult.Text = "%51.3 Walk";
+            }
+            //Cycle için-------------------//
+            if (Age > 6 && Age <= 11 && Dist > 0.8 && Dist <= 1.3 && Friend >= 0 && Friend <= 2)//yeni eklediklerim
+            {
+                lblResult.Text = "%59 Cycle";
+            }
+            if (Age >= 9 && Age <= 15 && Dist >= 0 && Dist < 0.8 && Friend >= 0 && Friend < 3)
+            {
+                lblResult.Text = "%54 Cycle";
+            }
+            if (Age >= 14 && Age <= 20 && Dist > 0 && Dist < 3 && Friend >= 0 && Friend < 800)
+            {
+                lblResult.Text = "%50.4 Cycle";
+            }
+            if (Age >= 20 && Age <= 30 && Dist > 3 && Dist < 4.2 && Friend >= 0 && Friend < 800)
+            {
+                lblResult.Text = "%87 Car";
+            }
+
+        }
+    }
+}
---
+++ JourneyTheSchoolv4/JourneytoSchool/JourneytoSchool/TrainForm.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace JourneytoSchool
+{
+    partial class TrainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TrainForm));
+            this.groupBoxInfo = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnAddExample = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBoxSchool1 = new System.Windows.Forms.PictureBox();
+            this.groupBoxInfo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSchool1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBoxInfo
+            // 
+            this.groupBoxInfo.Controls.Add(this.dataGridView1);
+            this.groupBoxInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBoxInfo.Location = new System.Drawing.Point(16, 226);
+            this.groupBoxInfo.Name = "groupBoxInfo";
+            this.groupBoxInfo.Size = new System.Drawing.Size(899, 251);
+            this.groupBoxInfo.TabIndex = 1;
+            this.groupBoxInfo.TabStop = false;
+            this.groupBoxInfo.Text = "Information";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.ActiveBorder;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 26);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(893, 222);
+            this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // btnAddExample
+            // 
+            this.btnAddExample.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnAddExample.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnAddExample.Location = new System.Drawing.Point(16, 483);
+            this.btnAddExample.Name = "btnAddExample";
+            this.btnAddExample.Size = new System.Drawing.Size(183, 64);
+            this.btnAddExample.TabIndex = 4;
+            this.btnAddExample.Text = "Add a Example";
+            this.btnAddExample.UseVisualStyleBackColor = false;
+            this.btnAddExample.Click += new System.EventHandler(this.btnAddExample_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnDelete.Location = new System.Drawing.Point(219, 483);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(183, 64);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "Delete a Example";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(863, 14);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(65, 49);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.pictureBoxSchool1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(-2, -2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(931, 212);
+            this.panel1.TabIndex = 7;
+            // 
+            // pictureBoxSchool1
+            // 
+            this.pictureBoxSchool1.BackColor = System.Drawing.Color.White;
+            this.pictureBoxSchool1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxSchool1.Image")));
+            this.pictureBoxSchool1.Location = new System.Drawing.Point(110, 14);
+            this.pictureBoxSchool1.Name = "pictureBoxSchool1";
+            this.pictureBoxSchool1.Size = new System.Drawing.Size(719, 198);
+            this.pictureBoxSchool1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxSchool1.TabIndex = 7;
+            this.pictureBoxSchool1.TabStop = false;
+            // 
+            // TrainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(927, 563);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.groupBoxInfo);
+            this.Controls.Add(this.btnAddExample);
+            this.Name = "TrainForm";
+            this.Text = "TrainForm";
+            this.Load += new System.EventHandler(this.TrainForm_Load);
+            this.groupBoxInfo.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSchool1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBoxInfo;
+        private System.Windows.Forms.Button btnAddExample;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBoxSchool1;
+    }
+}
